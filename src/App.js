bc7bda3770a19a7798d6596b695983e77a64c8b8
LEE JI SU <afafmm1903@gmail.com>
2025-05-14 18:13:12 +0900

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import MyIntro from './components/MyIntro';
-import MyMbti from './components/MyMbti';
-import MyName from './components/MyName';
-import LikeImg from './components/LikeImg';
+import React from "react";
+import MyIntro from "./components/MyIntro";
+import MyMbti from "./components/MyMbti";
+import MyName from "./components/MyName";
+import LikeImg from "./components/LikeImg";
 
 const App = () => {
-  const intro = "안녕하세요 풀스택6기 이지수입니다.";
+  const intro = "안녕하세요 풀스택6기 이지수입니다asdas.";
   const mbti = "ISFJ";
   const name = "이지수";
-  const imgUrl = "https://image.news1.kr/system/photos/2012/5/15/142764/article.jpg/dims/optimize"; // 예시 이미지 URL
+  const imgUrl =
+    "https://image.news1.kr/system/photos/2012/5/15/142764/article.jpg/dims/optimize"; // 예시 이미지 URL
   const altText = "LikeImg";
 
   return (
